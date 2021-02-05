a4dab77f8c3a2abeb67d78ed0528c49fb285b36a
Francis Gougeon <francgeon@gmail.com>
2021-02-05 14:39:03 -0500

    This is the initial commit.

---
+++ Sentiment Analysis/Sentiment_Analysis.py
@@ -0,0 +1,15 @@
+
+#!/usr/bin/env python3
+import datetime
+
+the_name = "Francis"
+the_time = str(datetime.datetime.now())[11:19]
+the_victory_cry = "a réussi à faire tourner son premier script"
+
+print()
+print()
+print(f"Il est {the_time} et {the_name} {the_victory_cry}!")
+print()
+print()
+
+
